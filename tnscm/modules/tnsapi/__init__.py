7b18c56ecfd484aa757748508425d7873d0aaedd
Damian Krawczyk <damian.krawczyk@limberduck.org>
2021-09-02 19:42:53 +0200

    v. 0.0.4

--- tnscm/modules/tnsapi/__init__.py
+++ tnscm/modules/tnsapi/__init__.py
@@ -104,6 +104,10 @@ class TnsApi:
         data = self.connect('GET', '/policies')['policies']
         return data
 
+    def policies_delete(self, id):
+        data = self.connect('DELETE', '/policies/{0}'.format(id))
+        return data
+
     def users_get(self):
         data = self.connect('GET', '/users')['users']
         return data
@@ -116,6 +120,14 @@ class TnsApi:
         data = self.connect('GET', '/scans')['scans']
         return data
 
+    def scan_delete(self, id):
+        data = self.connect('DELETE', '/scans/{0}'.format(id))
+        return data
+
     def plugins_families_get(self):
         data = self.connect('GET', '/plugins/families')['families']
+        return data
+
+    def settings_advanced_get(self):
+        data = self.connect('GET', '/settings/advanced')['preferences']
         return data
