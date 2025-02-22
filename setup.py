f5684af883526cefed624a18dba0dfe8b92b12e2
Damian Krawczyk <damian@damiankrawczyk.com>
2025-02-22 13:58:12 +0100

    v0.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     required = f.read().splitlines()
 
 about = {}
@@ -13,7 +13,7 @@ with open("tnscm/_version.py") as f:
 setuptools.setup(
     name="tnscm",
     version=about["__version__"],
-    license='MIT',
+    license="MIT",
     author="Damian Krawczyk",
     author_email="damian.krawczyk@limberduck.org",
     description="TNSCM (Tenable Nessus CLI Manager) by LimberDuck",
@@ -22,15 +22,14 @@ setuptools.setup(
     url="https://github.com/LimberDuck/tnscm",
     packages=setuptools.find_packages(),
     install_requires=required,
-    entry_points={
-        "console_scripts": [
-            "tnscm = tnscm.__main__:main"
-        ]
-    },
+    entry_points={"console_scripts": ["tnscm = tnscm.__main__:main"]},
     classifiers=[
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
